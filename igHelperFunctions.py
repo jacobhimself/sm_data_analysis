56756e0b2168d4147932c9c93163801b2aeb6590
Jacob <jacobfrance28@gmail.com>
2023-11-21 11:47:16 +1100

    have completed sections 2, 3, and 4 of planned data movement as per README doc. Will now move on to learning Google Apps Script to automate the upload of csv file each day

--- igHelperFunctions.py
+++ igHelperFunctions.py
@@ -9,20 +9,27 @@ def createIgSession(userName, password):
     return session
 
 #Given a csv file of instagram handles, return a dictionary with all keys set to -1
-def populateDefaultDict(igHandleCSV):
+def populateDefaultDict(igHandleCSV_filename):
     tempDict = {}
-    with open(igHandleCSV, 'r') as f:
+    with open(igHandleCSV_filename, 'r') as f:
     #remove line break and add key:value pair to dictionary
         for line in f:
             tempDict[line.strip()] = -1
     f.close()
     return tempDict
 
+#Given a dict with instagram handles as keys, populate the values from instagram
+def populateFollowerDict(handleDictIn, igSession):
+    for handle in handleDictIn.keys():
+        dl_user = handle
+        print("Gathering data for ", dl_user)
+        handleDictIn[handle] = instaloader.Profile.from_username(igSession.context, dl_user).followers
+        # going to try without return incase handleDictIn is global?
 
 # ADD CONTENTS OF RIDER : FOLLOWER DICT TO TODAYS_DATE.CSV
 def saveDictToCSV(dictIn):
-    datetimeAsString = str(date.today())
-    outputFileName = datetimeAsString + ".csv"
+    dateAsString = str(date.today())
+    outputFileName = dateAsString + ".csv"
 
     with open(outputFileName,'w') as f:
         w = csv.writer(f)
